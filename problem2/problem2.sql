86c8d8a2f75273b0883197b861662cf73c09f27f
Kent Chen <kent831213@gmail.com>
2022-06-15 21:22:04 -0500

    Update problem2.sql

--- problem2/problem2.sql
+++ problem2/problem2.sql
@@ -8,6 +8,7 @@ VALUES(456,123456789,"Doe",'2020-03-22');
 SELECT *
 FROM customer;
 
+INSERT INTO customer(ssn,id,first_name,last_name,favorite_color,last_updated)
 SELECT c.ssn,
 	MAX(CASE WHEN id_num = 1 THEN id END) AS id,
     MAX(CASE WHEN first_name_num = 1 THEN first_name END) AS first_name,
@@ -16,12 +17,19 @@ SELECT c.ssn,
     CURRENT_TIMESTAMP() AS last_updated
 FROM (SELECT c.*,
 		ROW_NUMBER() OVER(PARTITION BY ssn 
-							ORDER BY (CASE WHEN id IS NOT NULL THEN 0 ELSE 1 END), last_updated DESC) AS id_num,
+							ORDER BY  last_updated DESC) AS id_num,
         ROW_NUMBER() OVER(PARTITION BY ssn 
-							ORDER BY (CASE WHEN first_name IS NOT NULL THEN 0 ELSE 1 END), last_updated DESC) AS first_name_num,                    
+							ORDER BY (CASE WHEN first_name IS NOT NULL THEN 1 ELSE 2 END), last_updated DESC) AS first_name_num,                    
 		ROW_NUMBER() OVER(PARTITION BY ssn 
-							ORDER BY (CASE WHEN last_name IS NOT NULL THEN 0 ELSE 1 END), last_updated DESC) AS last_name_num,    
+							ORDER BY (CASE WHEN last_name IS NOT NULL THEN 1 ELSE 2 END), last_updated DESC) AS last_name_num,    
 		ROW_NUMBER() OVER(PARTITION BY ssn 
-							ORDER BY (CASE WHEN favorite_color IS NOT NULL THEN 0 ELSE 1 END), last_updated DESC) AS favorite_color_num
+							ORDER BY (CASE WHEN favorite_color IS NOT NULL THEN 1 ELSE 2 END), last_updated DESC) AS favorite_color_num
 		FROM customer AS c) AS c
 GROUP BY ssn;
+
+DELETE FROM customer as c1
+WHERE last_updated<(
+	SELECT * FROM(
+	SELECT MAX(last_updated)
+    FROM customer as c2
+    WHERE c1.ssn = c2.ssn) as temp);
