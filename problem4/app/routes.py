d70fccee5791e7d6241427af6be6b2fef3686baa
Kent Chen <kent831213@gmail.com>
2022-06-18 19:50:21 -0500

    modify problem4

--- problem4/app/routes.py
+++ problem4/app/routes.py
@@ -16,11 +16,5 @@ def redirected(name,number=0):
     number = request.form.get('number')
     return render_template("index.html",name=name,number=number)
 
-# @app.route("/ceasar-cipher/<string:name>", methods=["POST","GET"])
-# def product_search(name):
 
-#     get_number = request.form.get('number')
-#     print(name)
-#     print(get_number)
-#     return redirect(url_for('redirected',name=name,number=get_number))
 
